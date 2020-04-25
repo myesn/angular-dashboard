fe4a1c641173916411a0e75042363d31b7a529cd
myesn <myesn@foxmail.com>
2020-04-26 01:22:56 +0800

    first commit

---
+++ src/app/layouts/default/default.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-default',
+  templateUrl: './default.component.html',
+  styleUrls: ['./default.component.scss']
+})
+export class DefaultComponent implements OnInit {
+
+  sidebarOpen = true;
+
+  constructor() { }
+
+  ngOnInit(): void { }
+
+  sidebarToggler() {
+    this.sidebarOpen = !this.sidebarOpen;
+  }
+}
